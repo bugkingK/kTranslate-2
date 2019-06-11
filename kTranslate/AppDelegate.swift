686a55fdda3da537bf838a020eaa0026e6792a3d
bugkingK <myway0710@naver.com>
2019-06-11 12:55:37 +0900

    Initial Commit

---
+++ kTranslate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  kTranslate
+//
+//  Created by moon on 11/06/2019.
+//  Copyright © 2019 bugking. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
